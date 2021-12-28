563d64e66bafbd77b81e8f03baeeb85eb25322dc
Sakthivel Palani <sakthivel.palani@syncfusion.com>
2021-12-28 14:37:50 +0530

    All files

---
+++ wwwroot/scripts/common/sidebar.js
@@ -0,0 +1,14 @@
+﻿function onTocClick(args) {
+    let reportViewerElement = document.querySelector('.e-reportviewer.e-js');
+    if (reportViewerElement)
+        $(reportViewerElement).boldReportViewer("clearReportCache");
+    args.e.preventDefault();
+    let curSampleData = reportSamples[args.index];
+    let reportPath = curSampleData.routerPath ? (curSampleData.basePath + '/' + curSampleData.routerPath) : curSampleData.basePath;
+    location.href = location.origin + getBasePath() + reportPath;
+}
+
+function onHomeBtnClick() {
+    let homePageUrl = location.origin.indexOf('demos.boldreports.com') !== -1 ? '/home/' : '/';
+    location.href = location.origin + homePageUrl + 'aspnet-core.html';
+}
